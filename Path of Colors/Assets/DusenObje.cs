ce6a896fff9b9c77626d6b41ec0011bd09ae1be2
Abdullah Aydın <abdayd2171@gmail.com>
2024-05-29 21:44:03 +0300

    Level 7

---
+++ Path of Colors/Assets/DusenObje.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DusenObje : MonoBehaviour
+{
+    public Rigidbody2D targetRigidbody; // Deðiþtirilecek Rigidbody2D bileþeni
+
+    // Nesne, belirli bir Collider ile çarpýþtýðýnda tetiklenecek olay
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Çarpýþma yapan nesnenin "Player" etiketine sahip olup olmadýðýný kontrol ederiz
+        if (collision.CompareTag("Player"))
+        {
+            // Hedef Rigidbody'nin bodyType'ýný Kinematic'ten Dynamic'e deðiþtiririz
+            targetRigidbody.bodyType = RigidbodyType2D.Dynamic;
+        }
+    }
+}
