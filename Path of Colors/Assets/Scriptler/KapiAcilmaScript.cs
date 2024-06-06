1884f98048170d5ab7a09d03a8a579f32f1e00d0
Abdullah Aydın <abdayd2171@gmail.com>
2024-06-07 00:07:42 +0300

    Level2.1

---
+++ Path of Colors/Assets/Scriptler/KapiAcilmaScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KapiAcilmaScript : MonoBehaviour
+{
+    public Animator SolKapi;
+    public Animator SagKapi;
+    public Animator IsikAnim;
+    public Animator IsikAnim1;
+    public Animator IsikAnim2;
+    private bool playerInRange = false;
+    Animator anim;
+
+void Start () 
+{
+    anim.GetComponent<Animator>();
+}
+
+    void Update()
+    {
+        // Eğer oyuncu düğme alanındaysa ve 'E' tuşuna basarsa
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            SolKapi.SetBool("Open", true);
+            SagKapi.SetBool("Open",true);
+            IsikAnim.SetBool("Yesil",true);    
+            IsikAnim1.SetBool("Yesil",true);
+            IsikAnim2.SetBool("Yesil",true);        
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;   
+        }
+    }
+}
+
--- Path of Colors/Assets/Scriptler/Kopru Scriptler/ButtonScript.cs
+++ Path of Colors/Assets/Scriptler/Kopru Scriptler/ButtonScript.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class ButtonScript : MonoBehaviour
 {
+    public Animator Isik_1;
+    public Animator Isik_2;
     public KopruScript brindge;
     private bool playerInRange = false;
-
+     Animator anim;
+void Start()
+{
+    anim.GetComponent<Animator>();
+}
     void Update()
     {
         // Eğer oyuncu düğme alanındaysa ve 'E' tuşuna basarsa
         if (playerInRange && Input.GetKeyDown(KeyCode.E))
         {
             brindge.FlattenBridge();
+            Isik_1.SetBool("Yesil",true);
+            Isik_2.SetBool("Yesil",true);   
+
         }
     }
 
